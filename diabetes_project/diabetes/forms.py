96e226fe6313466fc03fbe84c32c3f43f8ccd87f
hrebinets1 <grebinec.oleksandr@gmail.com>
2025-12-14 17:30:06 +0200

    add missing files and localization

--- diabetes_project/diabetes/forms.py
+++ diabetes_project/diabetes/forms.py
@@ -5,14 +5,19 @@ from django.contrib.auth import get_user_model
 CustomUser = get_user_model()
 
 class AuthMedicForm(forms.Form):
-    username = forms.CharField(label='Enter name', max_length=25)
-    password = forms.CharField(label='Enter password', max_length=30, widget=forms.PasswordInput)
+    username = forms.CharField(label="Впишіть ім'я", max_length=25)
+    password = forms.CharField(label="Впишіть пароль", max_length=30, widget=forms.PasswordInput)
 
 class RegisterMedicForm(forms.ModelForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'password', 'first_name', 'last_name', 'hospital')
+        labels = {
+            'first_name': "Ваше ім'я",
+            'last_name': "Ваше прізвище",
+            'hospital': "Ваша лікарня"
+        }
 
-    username = forms.CharField(label="Enter name", max_length=25)
-    email = forms.EmailField(label='Enter email', max_length=50)
-    password = forms.CharField(label='Enter password', max_length=30)
+    username = forms.CharField(label="Впишіть логін", max_length=25)
+    email = forms.EmailField(label='Впишіть пошту', max_length=50)
+    password = forms.CharField(label='Впишіть пароль', max_length=30)
