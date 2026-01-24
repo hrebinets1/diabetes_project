9a5fb1b97eaf6a36bc5275a52ee931ae3fdd1074
hrebinets1 <grebinec.oleksandr@gmail.com>
2026-01-24 12:56:21 +0200

    [MEDIC]: edit tests.py (medic tests)

--- diabetes_project/diabetes/tests.py
+++ diabetes_project/diabetes/tests.py
@@ -47,15 +47,45 @@ class MedicTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Oleksandr")
-        self.assertContains(response, "10") # create in setUp function
+        self.assertContains(response, "10") # created earlier in setUp function
         self.assertContains(response, "5.5")
 
-    def test_unique_patient_info_on_medic_page(self):
-        med_pass = "unique_password_321"
+    def test_medic_shows_patient_glucose_context(self):
+        self.client.login(username=self.medic.username, password=self.medic_password)
+        #old info
+        GlucoStats.objects.create(
+            user=self.patient,
+            level=5.0,
+            context="normal",  # Натщесерце
+            measurement_date="2026-01-24T10:00:00.000+00:00"
+        )
+
+        response = self.client.get(reverse('main_medic_page'))
+        patient_data = response.context['patients'][0]
+
+        self.assertEqual(float(patient_data['last_glucose']), 5.0)
+        self.assertEqual(patient_data['context_display'], 'Натщесерце')
+
+        #new info
+        GlucoStats.objects.create(
+            user=self.patient,
+            level=9.0,
+            context="post_meal",  # Після їжі
+            measurement_date="2026-01-24T12:00:00.000+00:00"
+        )
+
+        response = self.client.get(reverse('main_medic_page'))
+        patient_data = response.context['patients'][0]
+
+        self.assertEqual(float(patient_data['last_glucose']), 9.0)
+        self.assertEqual(patient_data['context_display'], 'Після їжі')
+
+    def test_no_patient_info_on_medic_page(self):
+        med_pass = "password_123"
         med = CustomUser.objects.create_user(
-            username="unique_medic_test",
+            username="new_medic",
             role="medic",
-            hospital="Unique Hospital",
+            hospital="Hospital",
             password=med_pass
         )
         self.client.login(username=med.username, password=med_pass)
